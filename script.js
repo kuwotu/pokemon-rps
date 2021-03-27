dd2d57930cb8e4a772bb4ae7ff3096c8a7396a5a
Kevwe Uwotu <kevwe1996@gmail.com>
2021-03-27 21:36:46 +0000

    image fix

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ pokeball = () => {
   const imgPokeball = document.querySelector("#img-pokeball");
   imgPokeball.classList.add("pokeball-refresh");
   document.getElementById("img-pokeball").style.visibility = "visible";
-  document.getElementById("img-pokeball").src = "./images/Poké_Ball.png";
+  document.getElementById("img-pokeball").src = "./images/Poké_Ball.jpg";
 
   const refreshText = document.createElement("p");
   refreshText.classList.add("refresh-text");
